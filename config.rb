dc19b390a2ddae77eb62e4173858354ff6c28fdb
Jack Letourneau <jletourneau@users.noreply.github.com>
2017-03-18 12:00:27 -0400

    Changing layout and asset dirs.

--- config.rb
+++ config.rb
@@ -1,49 +1,19 @@
-###
-# Page options, layouts, aliases and proxies
-###
+config[:css_dir]    = 'assets/stylesheets'
+config[:js_dir]     = 'assets/javascripts'
+config[:images_dir] = 'assets/images'
+config[:fonts_dir]  = 'assets/fonts'
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+config[:layouts_dir]  = '_layouts'
+config[:partials_dir] = '_partials'
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# activate :sprockets
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-activate :sprockets
-
-# Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
+  activate :livereload, no_swf: true
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Cachebusting asset names
   activate :asset_hash
 end
